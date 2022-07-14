6d821112e0f5c78a954321df7acba00afca1adbb
BumbaruFlaviusGabriel <bumbaruflavius271@gmail.com>
2022-07-14 13:41:39 +0300

    new commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup>
-import PageContainer from '../components/PageContainer.vue';
+import TodoList from '../components/TodoList.vue';
 </script>
 
 <template>
-    <PageContainer title="Home page" />
+    <TodoList/>
 </template>
